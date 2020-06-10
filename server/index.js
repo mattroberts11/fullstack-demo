ac05a43908dd956f9ed5da5222d1cd82823ea313
Matt Roberts <justskiit@gmail.com>
2020-06-10 14:11:55 -0600

    Post working

--- server/index.js
+++ server/index.js
@@ -1,1 +1,29 @@
 const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const bp = require('body-parser');
+const cors = require('cors')
+//include models
+const Bug = require('./db/schemas.js');
+
+app.use(cors());
+app.use(bp.json());
+app.use(bp.urlencoded({extended: false}));
+
+
+app.post('/api/bugs', (req, res) => {
+  let postBug = new Bug(req.body)
+  postBug.save( (err) => {
+    if(err) console.log('Error on post');
+  })
+  res.status(201).send(postBug);
+})
+
+
+
+app.listen(port, () => {
+  console.log("DB Server listing on port " + port);
+});
+
+module.exports = app;
