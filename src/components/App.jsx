e4cbcf77bc269dc96283de9585388a61c815c963
Matt Roberts <justskiit@gmail.com>
2020-06-10 17:04:14 -0600

    Modal working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Nav from './Nav.jsx';
 import BugTile from './BugTile.jsx';
 import exampleData from '../example-data/exampleData';
+import Modal from './Modal.jsx';
 
 import '../styles/App.scss';
 
@@ -12,13 +14,24 @@ class App extends React.Component {
       filter: 'None',
       // bugs: exampleData,
       bugs: [],
+      show: false
     };
     this.filterHandler = this.filterHandler.bind(this);
     this.getBugs = this.getBugs.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
   }
 
   componentDidMount(){
     this.getBugs();
+  }
+
+  showModal() {
+    this.setState({show: true});
+  }
+
+  hideModal() {
+    this.setState({show: false});
   }
 
   filterHandler(filter) {
@@ -35,6 +48,15 @@ class App extends React.Component {
 
   render() {
     return (
+      <main>
+      <Modal show={this.state.show} handleClose={this.hideModal}>
+        <p>Model</p>
+        <p>Data</p>
+      </Modal>
+      <button type="button" onClick={this.showModal}>
+        Open
+      </button>
+
       <table>
         <Nav
           filterHandler={this.filterHandler}
@@ -69,21 +91,9 @@ class App extends React.Component {
                 ))
         }
       </table>
+      </main>
     );
   }
 }
 
 export default App;
-
-
-
-// this.state.bugs.map((bug) => (
-//   <BugTile
-//     bugName={bug.bugName}
-//     bugDescription={bug.bugDescription}
-//     reportedBy={bug.reportedBy}
-//     createdDate={bug.createdDate}
-//     assignedTo={bug.assignedTo}
-//     threatLevel={bug.threatLevel}
-//     key={bug.bugName}
-//   />
